89d5a78f8a5a978e2b172c33482573fc3b22bf6a
Daniel Sampliner <samplinerD@gmail.com>
2023-06-12 12:21:26 -0400

    feat: initial commit

---
+++ flake.nix
@@ -0,0 +1,34 @@
+# SPDX-FileCopyrightText: 2023 Daniel Sampliner <samplinerD@gmail.com>
+#
+# SPDX-License-Identifier: GLWTPL
+
+{
+  description = "nix containers";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils/v1.0.0";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+
+    devshell.url = "github:numtide/devshell";
+    devshell.inputs.nixpkgs.follows = "nixpkgs";
+
+    pre-commit-hooks-nix.url = "github:cachix/pre-commit-hooks.nix";
+    pre-commit-hooks-nix.inputs.nixpkgs-stable.follows = "nixpkgs";
+    pre-commit-hooks-nix.inputs.flake-utils.follows = "flake-utils";
+    pre-commit-hooks-nix.inputs.nixpkgs.follows = "unstable";
+  };
+
+  outputs = { flake-parts, flake-utils, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      debug = true;
+      systems = [ flake-utils.lib.system.x86_64-linux ];
+
+      imports = [
+        ./fp/devshell.nix
+      ];
+    };
+}
---
+++ fp/devshell.nix
@@ -0,0 +1,73 @@
+# SPDX-FileCopyrightText: 2023 Daniel Sampliner <samplinerD@gmail.com>
+#
+# SPDX-License-Identifier: GLWTPL
+
+{ inputs, perSystem, ... }: {
+  imports = builtins.catAttrs "flakeModule" (builtins.attrValues {
+    inherit (inputs)
+      devshell
+      pre-commit-hooks-nix
+      ;
+  });
+
+  perSystem = { config, lib, pkgs, ... }:
+    {
+      devshells.default.devshell = {
+        motd = "";
+        name = "nix-containers";
+
+        packages =
+          let
+            enabledPCHooks = lib.filterAttrs (_: v: v.enable)
+              config.pre-commit.settings.hooks;
+
+            pcPkgs = [ config.pre-commit.settings.package ]
+              ++ lib.attrVals
+              (builtins.attrNames enabledPCHooks)
+              config.pre-commit.settings.tools
+            ;
+          in
+          pcPkgs;
+
+        startup.pre-commit.text = config.pre-commit.installationScript;
+      };
+
+      pre-commit.settings =
+        let
+          inherit (pkgs.python3.pkgs) pre-commit-hooks;
+        in
+        {
+          hooks = {
+            commitizen.enable = true;
+            deadnix.enable = true;
+            editorconfig-checker.enable = true;
+            nixpkgs-fmt.enable = true;
+            statix.enable = true;
+            taplo.enable = true;
+
+            end-of-file-fixer = {
+              enable = true;
+              name = "fix end of files";
+              entry = "${pre-commit-hooks}/bin/end-of-file-fixer";
+              types = [ "text" ];
+            };
+
+            reuse = {
+              enable = true;
+              name = "REUSE license compliance";
+              entry = "${pkgs.reuse}/bin/reuse lint";
+              pass_filenames = false;
+            };
+          };
+
+          tools = {
+            inherit (pkgs)
+              reuse
+              taplo
+              ;
+
+            end-of-file-fixer = pre-commit-hooks;
+          };
+        };
+    };
+}
