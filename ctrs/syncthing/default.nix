9a9afa64b97883fc9f1dd45f72dc4f1422b5a886
Daniel Sampliner <samplinerD@gmail.com>
2023-12-18 22:48:57 -0500

    feat(syncthing): add syncthing container

---
+++ ctrs/syncthing/default.nix
@@ -0,0 +1,57 @@
+# SPDX-FileCopyrightText: 2023 Daniel Sampliner <samplinerD@gmail.com>
+#
+# SPDX-License-Identifier: GLWTPL
+
+{ dockerTools
+, curl
+, syncthing
+}:
+let
+  name = syncthing.pname;
+in
+dockerTools.streamLayeredImage {
+  inherit name;
+  tag = syncthing.version;
+
+  contents = [
+    curl
+    dockerTools.caCertificates
+    syncthing
+  ];
+
+  config = {
+    Cmd = [
+      "serve"
+      "--no-default-folder"
+      "--no-browser"
+      "--no-restart"
+    ];
+    Entrypoint = [ "syncthing" ];
+    Env = [
+      "PATH=/bin"
+      "STCONFDIR=/config"
+      "STDATADIR=/data"
+    ];
+    ExposedPorts = {
+      "21027/udp" = { };
+      "22000/tcp" = { };
+      "22000/udp" = { };
+    };
+    Healthcheck = {
+      Test = [
+        "CMD"
+        "curl"
+        "-fsSk"
+        "https://localhost:8384/rest/noauth/health"
+      ];
+    };
+    Labels = {
+      "org.opencontainers.image.source" =
+        "https://github.com/becometheteapot/${name}";
+    };
+    Volumes = {
+      "/config" = { };
+      "/data" = { };
+    };
+  };
+}
