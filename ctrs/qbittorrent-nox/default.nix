1b3be5233fc5e0062274595742c27a8f8dafb20f
Daniel Sampliner <samplinerD@gmail.com>
2024-01-12 23:15:04 -0500

    feat(qbittorrent): healthcheck whether online

--- ctrs/qbittorrent-nox/default.nix
+++ ctrs/qbittorrent-nox/default.nix
@@ -5,6 +5,7 @@
 { dockerTools
 , coreutils
 , curl
+, is-online
 , lib
 , qbittorrent-nox
 , writeText
@@ -20,7 +21,7 @@ let
 
   entrypoint = writers.writeExecline { } "/entrypoint" ''
     importas -i path PATH
-    export PATH ${lib.makeBinPath [coreutils]}:$path
+    export PATH ${lib.makeBinPath [coreutils qbittorrent-nox]}:$path
     importas -i XDG_CONFIG_HOME XDG_CONFIG_HOME
     importas -i XDG_DATA_HOME XDG_DATA_HOME
     define confDir ''${XDG_CONFIG_HOME}/qBittorrent
@@ -44,16 +45,23 @@ let
     emptyenv -c
     stdbuf -oL qbittorrent-nox
   '';
+
+  healthcheck = writers.writeExecline { } "/entrypoint" ''
+    importas -i path PATH
+    export PATH ${lib.makeBinPath [curl is-online]}:$path
+
+    if { is-online }
+    curl -qsSf localhost:8080/api/v2/app/version
+  '';
 in
 dockerTools.streamLayeredImage {
   inherit name;
   tag = qbittorrent-nox.version;
 
   contents = [
-    entrypoint
-    coreutils
     dockerTools.caCertificates
-    qbittorrent-nox
+    entrypoint
+    healthcheck
   ];
 
   config = {
@@ -64,9 +72,7 @@ dockerTools.streamLayeredImage {
       "XDG_CACHE_HOME=/cache"
     ];
     ExposedPorts = { "8080/tcp" = { }; };
-    Healthcheck = {
-      Test = [ "CMD" "${curl}/bin/curl" "-qsSf" "localhost:8080/api/v2/app/version" ];
-    };
+    Healthcheck = { Test = [ "CMD" "/healthcheck" ]; };
     Labels = {
       "org.opencontainers.image.source" =
         "https://github.com/becometheteapot/${name}";
