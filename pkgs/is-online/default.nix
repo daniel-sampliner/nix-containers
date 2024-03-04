c6e61dec1f23902eeed41602376111592815f957
Daniel Sampliner <samplinerD@gmail.com>
2024-03-04 17:48:24 -0500

    feat(is-online): reduce max retry time

--- pkgs/is-online/default.nix
+++ pkgs/is-online/default.nix
@@ -8,7 +8,7 @@
 }: writers.writeExecline { } "/bin/is-online" ''
   backtick -E url { ${coreutils}/bin/shuf -n1 ${./endpoints} }
   backtick -E rc { ${curl}/bin/curl
-    --max-time 1 --retry 10 --retry-max-time 60
+    --max-time 1 --retry 10 --retry-max-time 10
     --fail --silent --show-error
     --write-out "%{response_code}\n" --output /dev/null $url }
 
