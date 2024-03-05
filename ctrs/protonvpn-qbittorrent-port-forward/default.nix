37a9df72bfe8257c3cfe7552ec2781d2e8a1eda6
Daniel Sampliner <samplinerD@gmail.com>
2024-03-05 12:09:45 -0500

    feat(protonvpn-qbittorrent-port-forward): add log statements

--- ctrs/protonvpn-qbittorrent-port-forward/default.nix
+++ ctrs/protonvpn-qbittorrent-port-forward/default.nix
@@ -25,6 +25,11 @@ let
       "natpmpc -g ${gateway}"
       + (if op == "check" then "" else " -a 1 0 ${op} 60");
 
+  curlCmd = "curl --fail --silent --show-error --max-time 3 --retry 10";
+  log = format: rest: ''foreground { fdmove -c 1 2 printf "${format}\n" ''
+    + lib.optionalString (rest != "") ''"${rest}" ''
+    + "}";
+
   mainLoop =
     let
       getPort = let awk = "${gawk}/bin/awk"; in writers.makeScriptWriter
@@ -43,8 +48,6 @@ let
 
           END { exit $ret }
         '';
-
-      curlCmd = "curl --fail --silent --show-error --max-time 3 --retry 5";
     in
     writers.writeExecline { } "/bin/mainloop" ''
       emptyenv -c
@@ -53,7 +56,9 @@ let
       importas -i -u bgPID !
 
       backtick -E port { pipeline { ${natpmpCmd "udp"} } ${getPort} }
-      if { ${curlCmd}
+      ${log ''received udp port: %d'' "$port"}
+
+      if { ${curlCmd} --retry-max-time 60
         --data-urlencode "json={\"listen_port\":''${port}}"
         localhost:8080/api/v2/app/setPreferences }
       if { pipeline { ${curlCmd} localhost:8080/api/v2/app/preferences }
@@ -62,42 +67,27 @@ let
       wait $bgPID
     '';
 
-  entrypoint =
-    let
-      runtimeInputs = [
-        coreutils
-        curl
-        jq
-        libnatpmp
-        snooze
-      ];
-    in
-    writers.writeExecline { } "/entrypoint" ''
-      importas -i path PATH
-      export PATH ${lib.makeBinPath runtimeInputs}:$path
+  entrypoint = writers.writeExecline { } "/bin/entrypoint" ''
+    if { ${curlCmd} --retry-max-time 30 --retry-connrefused
+      localhost:8080/api/v2/app/version }
+    if { printf "\n" }
+    ${log ''qBittorrent up!'' "" }
 
-      if { curl
-        --fail --silent --show-error --max-time 3 --retry 10 --retry-connrefused
-        localhost:8080/api/v2/app/version }
-      if { printf "\n" }
+    if { touch --date "60 seconds" /run/end }
+    if { loopwhilex -x 0,69
+      if { snooze -H* -M* -S* -t /run/now -T 1 }
+      if { touch /run/now }
+      ifelse -n { eltest /run/end -nt /run/now } { foreground { fdmove -c 1 2 printf "natpmp check timeout\n" } exit 69 }
+      timeout 10 ${natpmpCmd "check"} }
+    ${log ''natpmp OK!'' "" }
+    foreground { rm -- /run/end /run/now }
 
-      if { touch --date "60 seconds" /run/end }
-      if { loopwhilex -x 0,69
-        if { touch /run/now }
-        ifelse -n { eltest /run/end -nt /run/now } { foreground { fdmove -c 1 2 echo timeout } exit 69 }
-        timeout 10 ${natpmpCmd "check"} }
-      if { rm -- /run/end /run/now }
+    emptyenv -c
+    loopwhilex
+      snooze -H* -M* -S* -t ${portFile} -T 45 timeout 55 mainloop
+  '';
 
-      emptyenv -c
-      loopwhilex
-        if { snooze -H* -M* -S* -t ${portFile} -T 45 }
-        timeout 55 mainloop
-    '';
-
-  healthcheck = writers.writeExecline { } "/healthcheck" ''
-    importas -i path PATH
-    export PATH ${lib.makeBinPath [coreutils]}:$path
-
+  healthcheck = writers.writeExecline { } "/bin/healthcheck" ''
     if { eltest -f ${portFile} }
     if { touch -d -60seconds /run/last }
     eltest ${portFile} -nt /run/last
@@ -105,22 +95,27 @@ let
 in
 dockerTools.streamLayeredImage {
   inherit name;
-  tag = "0.0.4";
+  tag = "0.0.5";
 
   contents = [
     catatonit
+    coreutils
+    curl
     entrypoint
     healthcheck
+    jq
+    libnatpmp
     mainLoop
+    snooze
   ];
 
   config = {
-    Entrypoint = [ "/bin/catatonit" "-g" "--" "/entrypoint" ];
+    Entrypoint = [ "catatonit" "-g" "--" "entrypoint" ];
     Healthcheck = {
-      Test = [ "CMD" "/healthcheck" ];
+      Test = [ "CMD" "healthcheck" ];
       StartPeriod = 60 * 1000000000;
-      StartInterval = 5 * 1000000000;
+      StartInterval = 1 * 1000000000;
     };
-    Volumes = { "/data" = { }; };
+    Volumes = { "/run" = { }; };
   };
 }
